c51a8211fca7a855ed97868a36e3eb3b15bb1e07
JEEBAN95 <jeebankishore95@gmail.com>
2020-06-06 14:06:23 +0530

    initial commit

---
+++ src/main/java/com/st/ats/exception/handler/MailSendingIssueException.java
@@ -0,0 +1,31 @@
+package com.st.ats.exception.handler;
+/**
+ * this class behaves as custom Exception when any issue occurred in mail sending classes 
+ * 
+ * @author Jeeban
+ *
+ */
+public class MailSendingIssueException extends Exception {
+
+	
+	/**
+	 * this is serial version uid for this Exception
+	 */
+	private static final long serialVersionUID = 1L;	
+	/**
+	 * this is used to throw exception with message
+	 * @param msg
+	 */
+	public MailSendingIssueException(String msg) {
+		super(msg);
+	}
+	
+	/**
+	 * this is default exception for throw exception without 
+	 * any message
+	 */
+	public MailSendingIssueException() {
+	
+	}
+
+}
